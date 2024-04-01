7ec8ce23abeb9c0466a176f9d32929427d7e0e19
Sergey Kotelnikov <newelvin@gmail.com>
2024-04-01 12:02:48 +0300

    fix: add constants

---
+++ project/apps/blog/src/constants.ts
@@ -0,0 +1,3 @@
+export const GLOBAL_PREFIX = 'api';
+
+export const DEFAULT_PORT = 3000;
--- project/apps/blog/src/main.ts
+++ project/apps/blog/src/main.ts
@@ -7,15 +7,15 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { GLOBAL_PREFIX, DEFAULT_PORT } from './constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
+  app.setGlobalPrefix(GLOBAL_PREFIX);
+  const port = process.env.PORT || DEFAULT_PORT;
   await app.listen(port);
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+    `🚀 Application is running on: http://localhost:${port}/${GLOBAL_PREFIX}`
   );
 }
 
